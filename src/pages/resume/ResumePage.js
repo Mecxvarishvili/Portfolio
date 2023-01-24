f76c9506f4d9aa3994f4643e0e12ee395bf727ef
Your Name <vmecxvarishvili@gmail.com>
2023-01-24 23:55:24 +0400

    completed resuem page

--- src/pages/resume/ResumePage.js
+++ src/pages/resume/ResumePage.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import img from "./Vazha-Metskhvarishvili-1.jpg"
+import { Link } from 'react-router-dom';
+import { CONTACT_PAGE } from '../../components/routes';
 
 const ResumePage = () => {
     return (
-        <div>
+        <div className="resumeCont" >
             <div className="mainTitle" >resume</div>
-            <a href="https://drive.google.com/file/d/1jqOm65ozBVpYpvwUlCNuWvCXVk5sxO99/view" target="_blank" rel="noreferrer">download</a>
+            <div className="resumeAbout" >
+                Reach out to me via my
+                <Link to={CONTACT_PAGE} >contact page.</Link>
+                or
+                <a href="https://drive.google.com/file/d/1jqOm65ozBVpYpvwUlCNuWvCXVk5sxO99/view" target="_blank" rel="noreferrer">download</a>
+                the resume
+            </div>
+            <div className="resume-img">
+                <img src={img} alt="resume"/>
+            </div>
         </div>
     );
 };
