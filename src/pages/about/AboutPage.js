09ca47ab01660694b8cc4964869116d0ac6845c5
Your Name <vmecxvarishvili@gmail.com>
2023-01-27 14:57:10 +0400

    Completed about page

--- src/pages/about/AboutPage.js
+++ src/pages/about/AboutPage.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 const AboutPage = () => {
     return (
-        <div>
+        <div className="aboutPage" >
             <div className="mainTitle" >about</div>
-            
+            <div  >I'm Vazha Junior Front-end Developer located in Georgia</div>
+            <div className="skillCont" >
+                <div className="div1">Here are a few technologies I’ve been working with recently:</div>
+                <ul className='skills'>
+                    <li>HTML / CSS</li>
+                    <li>Javascript (ES6)</li>
+                    <li>React</li>
+                    <li>SCSS</li>
+                    <li>TypeScript</li>
+                    <li>Git</li>
+                </ul>
+            </div>
         </div>
     );
 };
