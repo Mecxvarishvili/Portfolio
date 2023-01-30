d6e20906fa53715f5aa897212841abdc3eec0769
Your Name <vmecxvarishvili@gmail.com>
2023-01-30 23:46:40 +0400

    Adding transition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Main from './pages/home/Home';
 import ProjectsPage from './pages/projects/ProjectsPage';
 import { ABOUT_PAGE, CONTACT_PAGE, HOME_PAGE, PROJECTS_PAGE, RESUME_PAGE} from './components/routes'
@@ -8,29 +8,43 @@ import Footer from './layouts/footer/Footer';
 import AboutPage from './pages/about/AboutPage';
 import ContactPage from './pages/contact/ContactPage';
 import ResumePage from './pages/resume/ResumePage';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [ isVisible, setVisible ] = useState(true)
+  const [ tr, setTr ] = useState("activetr")
+
+  const transparent = () => {
+    setTr("activetr")
+    setTimeout(() => {
+      setTr("")
+    }, 500)
+  }
+
+  useEffect(() => {
+    transparent()
+  }, [])
   
   return (
-    <Router>
-      <Header setVisible={setVisible} />
-        <div className='mainCont'>
-          {isVisible ? 
-          <div className="container">
-            <Routes>
-              <Route exact path={HOME_PAGE} element={<Main/>} />
-              <Route path={ABOUT_PAGE} element={<AboutPage />} />
-              <Route path={PROJECTS_PAGE} element={<ProjectsPage/>} />
-              <Route path={RESUME_PAGE} element={<ResumePage />} />
-              <Route path={CONTACT_PAGE} element={<ContactPage />} />
-            </Routes>
-             <Footer />
+    <div className={"transparent-website " + tr}>
+      <Router>
+        <Header setVisible={setVisible} />
+          <div className='mainCont'>
+            {isVisible ? 
+            <div className="container">
+              <Routes>
+                <Route exact path={HOME_PAGE} element={<Main/>} />
+                <Route path={ABOUT_PAGE} element={<AboutPage />} />
+                <Route path={PROJECTS_PAGE} element={<ProjectsPage/>} />
+                <Route path={RESUME_PAGE} element={<ResumePage />} />
+                <Route path={CONTACT_PAGE} element={<ContactPage />} />
+              </Routes>
+               <Footer />
+            </div>
+             : <></>}
           </div>
-           : <></>}
-        </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
