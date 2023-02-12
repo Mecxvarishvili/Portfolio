b4bb934e9bd8cadb079ef953d44dbcfb9a8faacc
Your Name <vmecxvarishvili@gmail.com>
2023-02-13 02:13:55 +0400

    change sizes for mobile

--- src/App.js
+++ src/App.js
@@ -20,16 +20,6 @@ function App() {
           <Route path={RESUME_PAGE} element={<AnimatedRoutes children={<ResumePage />} />} />
           <Route path={CONTACT_PAGE} element={<AnimatedRoutes children={<ContactPage />} />} />
         </Routes>
-        <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
- width="30.000000pt" height="24.000000pt" viewBox="0 0 30.000000 24.000000"
- preserveAspectRatio="xMidYMid meet">
-<metadata>
-Created by potrace 1.16, written by Peter Selinger 2001-2019
-</metadata>
-<g transform="translate(0.000000,24.000000) scale(0.100000,-0.100000)"
-fill="#000000" stroke="none">
-</g>
-</svg>
       </AnimatePresence>
   );
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { Link, useLoaderData } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { ABOUT_PAGE, CONTACT_PAGE, PROJECTS_PAGE, RESUME_PAGE } from '../../components/routes';
 
 const Main = () => {
 
     return (
-        <div className="homePage" 
-        initial={{opacity: 0}}
-        animate={{opacity: 1}}
-        exit={{opacity: 0}}
-        transition={{ delay: 0.5 }}>
+        <div className="homePage">
             <div className="name" >i'm vazha metskhvarishvili</div>
             <div className="dit1" >Junior Front-end Developer / React.js</div>
             <div className='routediv'>
